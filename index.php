f0f687d9aee7bb839009d54f5adcab7fe72a9ed4
KORAQI <besjan.koraqii@gmail.com>
2024-09-05 14:39:29 +0200

    Orgagnisez le code PHP en blocs avec footer

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     <!-- Le pied de page -->
     
     <footer id="pied_de_page">
-        <p>Copyright moi, tous droits réservés</p>
+        <?php include('footer.php'); ?>
     </footer>
     
     </body>
