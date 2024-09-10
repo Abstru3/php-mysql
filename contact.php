9c0f0b50f2e541dabe0ec82d4c42f95b4055d49a
KORAQI <besjan.koraqii@gmail.com>
2024-09-10 15:13:56 +0200

    Parametrez le formulaire d'envoi de fichier

--- contact.php
+++ contact.php
@@ -38,21 +38,26 @@
 
     <h1 class="my-4">Contactez-nous</h1>
 
-    <form action="submit_contact.php" method="post">
-        <div class="mb-3">
-            <label for="name" class="form-label">Nom</label>
-            <input type="text" class="form-control" id="name" name="name" required>
-        </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Adresse email</label>
-            <input type="email" class="form-control" id="email" name="email" required>
-        </div>
-        <div class="mb-3">
-            <label for="message" class="form-label">Message</label>
-            <textarea class="form-control" id="message" name="message" rows="4" required></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Envoyer</button>
-    </form>
+    <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
+    <div class="mb-3">
+        <label for="name" class="form-label">Nom</label>
+        <input type="text" class="form-control" id="name" name="name" required>
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Adresse email</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+    </div>
+    <div class="mb-3">
+        <label for="message" class="form-label">Message</label>
+        <textarea class="form-control" id="message" name="message" rows="4" required></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="screenshot" class="form-label">Votre capture d'écran</label>
+        <input type="file" class="form-control" id="screenshot" name="screenshot" />
+    </div>
+    <button type="submit" class="btn btn-primary">Envoyer</button>
+</form>
+
     
 </div>
 
