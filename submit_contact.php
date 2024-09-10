841595d03d14c4011a4645de0214e2d8b963ea26
KORAQI <besjan.koraqii@gmail.com>
2024-09-10 15:20:48 +0200

    Traitez l'envoi en PHP

--- submit_contact.php
+++ submit_contact.php
@@ -6,6 +6,25 @@ if (
     echo('<h1>Il faut un email et un message valides pour soumettre le formulaire.</h1>');
     return;
 }
+    // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+        if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] == 0)
+        {
+        // Testons si le fichier n'est pas trop gros
+        if ($_FILES['screenshot']['size'] <= 1000000)
+        {
+        // Testons si l'extension est autorisée
+        $fileInfo = pathinfo($_FILES['screenshot']['name']);
+        $extension = $fileInfo['extension'];
+        $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+        if (in_array($extension, $allowedExtensions))
+        {
+        // On peut valider le fichier et le stocker définitivement
+        move_uploaded_file($_FILES['screenshot']['tmp_name'],
+        'uploads/' . basename($_FILES['screenshot']['name']));
+        echo "L'envoi a bien été effectué !";
+        }
+    }
+}
 $email = htmlspecialchars($_POST['email']);
 $message = htmlspecialchars($_POST['message']);
 
@@ -28,7 +47,6 @@ $message = htmlspecialchars($_POST['message']);
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">Rappel de vos informations</h5>
-            
             <p class="card-text"><b>Email</b> : <?php echo $email; ?> </p>
             <p class="card-text"><b>Message</b> : <?php echo $message; ?> </p>
 
