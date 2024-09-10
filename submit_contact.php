9d5428374a6dd28b866f3c427d4cf951d8df8348
KORAQI <besjan.koraqii@gmail.com>
2024-09-10 14:30:24 +0200

    Testez la presence d'un parametre

--- submit_contact.php
+++ submit_contact.php
@@ -1,36 +1,32 @@
 <?php
-if (isset($_GET['email']) && isset($_GET['message'])) {
-    $email = htmlspecialchars($_GET['email']);
-    $message = htmlspecialchars($_GET['message']);
-} else {
-    header('Location: contact.php');
-    exit;
-}
+include_once('variables.php');
+include_once('functions.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Message bien reçu</title>
+    <title>Site de recettes - Page d'accueil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="d-flex flex-column min-vh-100">
 <div class="container">
     <?php include_once('header.php'); ?>
 
-    <h1 class="my-4">Message bien reçu !</h1>
+    <h1 class="my-4">Site de recettes</h1>
 
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Rappel de vos informations</h5>
-            <p class="card-text"><b>Email</b> : <?php echo $email; ?> </p>
-            <p class="card-text"><b>Message</b> : <?php echo $message; ?> </p>
-        </div>
-    </div>
+    <?php
+        if (!isset($_GET['email']) || !isset($_GET['message']))
+        {
+        echo('<h1>Il faut un email et un message pour soumettre le formulaire.</h1>');
+        // Arrête l'exécution de PHP
+        return;
+        }
+    ?>
 </div>
 
-<?php include_once('footer.php'); ?>
 </body>
 </html>
